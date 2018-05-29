bdbebdab5741332351ec2baffb1290dbed52abe3
Jonathan Mercado <jonathanmercado.dev@gmail.com>
2018-05-28 22:11:59 -0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,8 @@
+from twython import Twython, TwythonStreamer
+
+#api keys
+API_KEY = ''
+API_SECRET_KEY =  ''
+API_TOKEN = ''
+API_SECRET_TOKEN = ''
+api = Twython(API_KEY, API_SECRET_KEY, API_TOKEN, API_SECRET_TOKEN)
