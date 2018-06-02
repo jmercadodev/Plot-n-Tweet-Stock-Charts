67e808a99563478acac7eb1c34e699f9d8e6ebe1
Jonathan Mercado <jonathanmercado.dev@gmail.com>
2018-06-01 20:58:45 -0500

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,8 +1,8 @@
 from twython import Twython, TwythonStreamer
 
 #api keys
-API_KEY = ''
-API_SECRET_KEY =  ''
-API_TOKEN = ''
-API_SECRET_TOKEN = ''
+API_KEY = 'Your keys go between these two apostrophes'
+API_SECRET_KEY = 'Your keys go between these two apostrophes'
+API_TOKEN = 'Your keys go between these two apostrophes'
+API_SECRET_TOKEN = 'Your keys go between these two apostrophes'
 api = Twython(API_KEY, API_SECRET_KEY, API_TOKEN, API_SECRET_TOKEN)
